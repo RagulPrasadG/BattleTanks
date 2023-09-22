24a68717d39e9714429efb26b3a0adff08aec786
RagulPrasadG <gulshark572@gmail.com>
2023-09-22 14:24:33 +0530

    Tank Prefab created and is spawned in scene

---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject tankPrefab;
+    private void Start()
+    {
+        if(tankPrefab)
+        Instantiate(tankPrefab, this.transform.position, Quaternion.identity);
+    }
+}
