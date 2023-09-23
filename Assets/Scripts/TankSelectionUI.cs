62e701bf5a36ea20193d3be462bba94835387492
RagulPrasadG <gulshark572@gmail.com>
2023-09-23 14:39:51 +0530

    Created a Tank Selection UI

---
+++ Assets/Scripts/TankSelectionUI.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankSelectionUI : MonoBehaviour
+{
+    [SerializeField] TankSpawner tankSpawner;
+    public void OnBlueTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.Blue);
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnGreenTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.Green);
+        this.gameObject.SetActive(false);
+    }
+
+    public void OnRedTankSelected()
+    {
+        tankSpawner.CreateTank(TankTypes.Red);
+        this.gameObject.SetActive(false);
+    }
+
+}
--- Assets/Scripts/TankSpawner.cs
+++ Assets/Scripts/TankSpawner.cs
@@ -6,17 +6,26 @@ public class TankSpawner : MonoBehaviour
 {
     public List<Tank> tanksList;
     [SerializeField] TankView tankView;
-    private void Start()
+
+    public void CreateTank(TankTypes tankType)
     {
-        CreateTank();
+        switch (tankType)
+        {
+            case TankTypes.Red:
+                TankModel tankmodelred = new TankModel(tanksList[0].movementSpeed, tanksList[0].rotationSpeed, tanksList[0].color);
+                TankController tankControllerRed = new TankController(tankmodelred, tankView);
+                break;
+            case TankTypes.Green:
+                TankModel tankmodelgreen = new TankModel(tanksList[1].movementSpeed, tanksList[1].rotationSpeed, tanksList[1].color);
+                TankController tankControllerGreen = new TankController(tankmodelgreen, tankView);
+                break;
+            case TankTypes.Blue:
+                TankModel tankmodelblue = new TankModel(tanksList[2].movementSpeed, tanksList[2].rotationSpeed, tanksList[2].color);
+                TankController tankControllerBlue = new TankController(tankmodelblue, tankView);
+                break;
+        }
     }
 
-    private void CreateTank()
-    {
-        TankModel tankmodel = new TankModel(tanksList[0].movementSpeed, tanksList[0].rotationSpeed, tanksList[0].color);
-        TankController tankController = new TankController(tankmodel, tankView);
-
-    }
 }
 
 [System.Serializable]
